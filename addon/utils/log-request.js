199140ed002c5b2999301396dd451151ece9eca1
Robbie Pitts <robbie@sweatypitts.com>
2016-05-21 10:19:18 -0400

    Headers bug fix
    ACTUAL:
    
    `Content-Type` header is only set when other headers are set too.
    
    EXPECTED:
    
    `Content-Type` header is always set to `text/plain`.

--- addon/utils/log-request.js
+++ addon/utils/log-request.js
@@ -3,20 +3,19 @@ import fetch from 'ember-network/fetch';
 export default function LogRequest(opts) {
   this.url = opts.url;
   this.urls = opts.urls;
+  this.headers = opts.headers;
   this.useDifferentEndpoints = opts.useDifferentEndpoints;
 }
 
 LogRequest.prototype.send = function LogRequest_send(level, entry) {
   let url = this.url;
-  let headers = this.headers;
+  let headers = this.headers || {};
 
   if (this.useDifferentEndpoints) {
     url = this.urls[level];
   }
 
-  if (headers) {
-    headers['Content-Type'] = 'text/plain';
-  }
+  headers['Content-Type'] = 'text/plain';
 
   const fetchOptions = {
     method: 'POST',
--- tests/unit/utils/log-request-test.js
+++ tests/unit/utils/log-request-test.js
@@ -40,3 +40,40 @@ test('it sends the log entries to the `urls` if opted-in', function(assert) {
 
   subject.send('debug', 'Cool log entry');
 });
+
+test('it sets headers to headers given in options', function(assert) {
+  assert.expect(1);
+
+  let actualUrl = 'https://www.example.com/api/v1/log';
+  let options = {
+    url: actualUrl,
+    headers: {
+      'X-Whatever': 'foo bar'
+    }
+  };
+  let subject = new LogRequest(options);
+
+  subject.fetch = function stubbedFetch(_, fetchOptions) {
+    assert.equal(fetchOptions.headers['X-Whatever'], 'foo bar');
+    return Ember.RSVP.resolve({ ok: true });
+  };
+
+  subject.send('debug', 'Cool log entry');
+});
+
+test('it sets the `Content-Type` header to `text/plain`', function(assert) {
+  assert.expect(1);
+
+  let actualUrl = 'https://www.example.com/api/v1/log';
+  let options = {
+    url: actualUrl
+  };
+  let subject = new LogRequest(options);
+
+  subject.fetch = function stubbedFetch(_, fetchOptions) {
+    assert.equal(fetchOptions.headers['Content-Type'], 'text/plain');
+    return Ember.RSVP.resolve({ ok: true });
+  };
+
+  subject.send('debug', 'Cool log entry');
+});
